f2a90ee3c76993d6ca5e9dd6ada8afbbcc3e1846
chirimenmonster <chirimenmonster@users.noreply.github.com>
2016-11-29 18:59:14 +0900

    cleanup code

--- mod_saveaccount.py
+++ mod_saveaccount.py
@@ -4,26 +4,25 @@ import BigWorld
 from debug_utils import LOG_CURRENT_EXCEPTION
 from gui import GUI_SETTINGS
 
-MODNAME = 'SaveAccount'
+MOD_NAME = 'SaveAccount'
+MOD_VERSION = '1.1'
 
 def init():
 
     try:
-        BigWorld.logInfo(MODNAME, 'change settings to save account info', None)
+        BigWorld.logInfo(MOD_NAME, '{0} version {1}, mod to save account info'.format(MOD_NAME, MOD_VERSION), None)
         
         oldValue1 = GUI_SETTINGS.rememberPassVisible 
         oldValue2 = GUI_SETTINGS.clearLoginValue
 
-        settings = GUI_SETTINGS._GuiSettings__settings
-        settings['rememberPassVisible'] = True
-        settings['clearLoginValue'] = False
-        GUI_SETTINGS._GuiSettings__settings = settings
+        GUI_SETTINGS._GuiSettings__settings['rememberPassVisible'] = True
+        GUI_SETTINGS._GuiSettings__settings['clearLoginValue'] = False
         
         newValue1 = GUI_SETTINGS.rememberPassVisible 
         newValue2 = GUI_SETTINGS.clearLoginValue
 
-        BigWorld.logInfo(MODNAME, 'GUI_SETTINGS.rememberPassVisible={0}->{1}'.format(oldValue1, newValue1), None)
-        BigWorld.logInfo(MODNAME, 'GUI_SETTINGS.clearLoginValue={0}->{1}'.format(oldValue1, newValue2), None)
+        BigWorld.logInfo(MOD_NAME, 'GUI_SETTINGS.rememberPassVisible = {0} -> {1}'.format(oldValue1, newValue1), None)
+        BigWorld.logInfo(MOD_NAME, 'GUI_SETTINGS.clearLoginValue= {0} -> {1}'.format(oldValue2, newValue2), None)
         
     except:
         LOG_CURRENT_EXCEPTION()
